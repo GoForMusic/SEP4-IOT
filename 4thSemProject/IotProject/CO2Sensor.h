0c02f7cd5e8307361c93d30ba432326206e26163
Caticu <radumarian250797@gmail.com>
2022-12-10 14:49:41 +0100

    SEP4IOT-18

--- 4thSemProject/IotProject/CO2Sensor.h
+++ 4thSemProject/IotProject/CO2Sensor.h
@@ -1,8 +1,21 @@
 ﻿#pragma once
-#include <mh_z19.h>
+
+#include <stdint.h>
+#include <stdio.h>
+
 #include <ATMEGA_FreeRTOS.h>
 
+#include <task.h>
+#include <semphr.h>
+#include <stdbool.h>
+#include <serial.h>
+#include <mh_z19.h>
+#include "Setup.h"
 
-uint16_t getCO2();
-void initializeCO2();
-void measureCO2();
+
+void create(UBaseType_t Taskpriority);
+uint16_t getCo2();
+void initializeCo2Driver();
+void measureCo2();
+void Co2Task(void* pvpParameter);
+void createCo2Task(UBaseType_t Taskpriority);
