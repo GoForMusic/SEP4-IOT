c97abe797d3924702030e657f4b025d5046b5c9b
NothingToSayInHere <adrianus_20@yahoo.com>
2022-12-11 18:40:51 +0100

    handlers

--- 4thSemProject/IotProject/UpLinkHandler.h
+++ 4thSemProject/IotProject/UpLinkHandler.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <stdint.h>
+
+/**
+ * Create the UplinkHandler task
+ */
+
+void UplinkHandler_createTask(void);
+void UplinkHandlerTask(void);
+static void _lora_setup(void);
+static void reset_lora_module(void);
+
