f281139d9d6832409355a6416f174d3517187067
NothingToSayInHere <adrianus_20@yahoo.com>
2022-12-11 16:43:44 +0100

    downlink handler with servo to open/close box

---
+++ 4thSemProject/IotProject/DownLinkHandler.h
@@ -0,0 +1,5 @@
+void downLinkHandler_task_run();
+void downLinkHandler_task(void *pvParameters);
+void downLinkHandler_task_create(void);
+void rotateServoOpenBox(int8_t servoPin, int8_t percent);
+void rotateServoCloseBox(int8_t servoPin, int8_t percent);
