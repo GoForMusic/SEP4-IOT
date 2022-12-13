6c77c68abfd05a8d930e106009e1965df389a50d
Caticu <radumarian250797@gmail.com>
2022-12-13 16:54:37 +0100

    Added RCmotor.h and .c and fixed some things

---
+++ 4thSemProject/IotProject/LoRaWANHandler.h
@@ -0,0 +1,17 @@
+﻿//A header for class DownLinkHandler.c
+
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
+
+#include <ATMEGA_FreeRTOS.h>
+#include <task.h>
+#include <lora_driver.h>
+
+#include "message_buffer.h"
+#include "Setup.h"
+#include "Configuration.h"
+
+//Function for down link handler task creation
+void lora_downlink_handler_create(UBaseType_t lora_handler_task_priority);
